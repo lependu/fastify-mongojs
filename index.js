7e749fbd67a2bd242caacde1516559973774d7b7
lependu <lependu@protonmail.com>
2018-08-10 16:35:57 +0200

    Adds error handling

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const fp = require('fastify-plugin')
 const mongojs = require('mongojs')
 
 const fastifyMongojs = function (instance, opts, next) {
-  const db = mongojs('localhost:27017/db', ['a'])
+  const options = Object.assign({}, opts)
+
+  const client = opts.client || ''
+  delete opts.client
+
+  const collections = opts.collections || []
+  delete opts.collections
+
+  const db = mongojs(client, collections, opts)
+
+  db.on('error', function (err) {
+    return next(err)
+  })
 
   instance.decorate('mongo', db)
   instance.addHook('onClose', instance => instance.mongo.close())
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const plugin = require('.')
 
 const { test } = tap
 
-test('Decorates fastify instance with mongojs', t => {
+test('Decorates fastify instance with db | default options', t => {
   t.plan(4)
 
   register(t, {}, function (err, fastify) {
@@ -13,7 +13,37 @@ test('Decorates fastify instance with mongojs', t => {
 
     fastify.mongo.stats((err, res) => {
       t.error(err)
+      t.equal(res.db, 'admin')
+    })
+  })
+})
+
+test('Decorates fastify instance with db | custom options', t => {
+  t.plan(4)
+
+  register(t, { client: 'db', collections: ['a']}, function (err, fastify) {
+    t.error(err)
+    t.equal(typeof fastify.mongo, 'object')
+
+    fastify.mongo.stats((err, res) => {
+      t.error(err)
       t.equal(res.db, 'db')
+    })
+  })
+})
+
+test('Handles mongojs error', t => {
+  t.plan(3)
+
+  register(t, {
+    client: 'mongodb://1.0.0.1:27017/db?connectTimeoutMS=100',
+    collections: ['a']
+  }, function (err, fastify) {
+    t.error(err)
+
+    fastify.mongo.runCommand({ ping: 1 }, function (err) {
+      t.ok(err instanceof Error)
+      t.match(err.message, /failed to connect to server/)
     })
   })
 })
